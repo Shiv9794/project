ece458318d8ba0fcbd4c9898e8021b7276bb0570
Shivam singh <132449099+Shiv9794@users.noreply.github.com>
2025-04-12 10:40:48 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/feesmanagementsysteminjava/FeesManagementSystemInJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.feesmanagementsysteminjava;
+
+/**
+ *
+ * @author Shivam Singh
+ */
+public class FeesManagementSystemInJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
